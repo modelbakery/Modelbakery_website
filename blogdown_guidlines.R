d953bac2e542b10ffa624c0180d8fa6e6fc6000e
modelbakery <sung9741@gmail.com>
2021-10-27 22:11:20 +0900

    initial website setup

---
+++ blogdown_guidlines.R
@@ -0,0 +1,26 @@
+# R Blog Tutorial Quidlines 
+
+
+# 1.0 Step up ----
+# install.packages('blogdown')
+# install_hugo()
+# new_site(theme = "wowchemy/starter-academic") # copied the theme from repository 
+
+
+# start_server()
+# stop_server()
+
+# 2.0 .gitignore ----
+# go to .gitignore *Add these to ignore*
+# .DS_Store
+#  Thumbs.db
+# 
+# commend
+# check_gitignore() *Check* whether they have been removed and checked*
+
+# go to .gitignore *Add these to ignore*
+# /public
+# /resources
+
+# commend 
+# check_content()
