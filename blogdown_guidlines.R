2cb1e277da356de00f826a5af1f94ebdc7aef379
modelbakery <sung9741@gmail.com>
2021-10-29 12:49:50 +0900

    libraries

--- blogdown_guidlines.R
+++ blogdown_guidlines.R
@@ -24,3 +24,13 @@
 
 # commend 
 # check_content()
+
+
+
+> A distribution is simply a collection of data, or scores, on a variable. Usually, these scores are arranged in order from smallest to largest and then they can be presented graphically. 
+
+--- Page 6, Statistics in Plain English, Third Edition, 2010.
+
+In the context of marketing, this argues that every customers are its unique score itself in a given dimensional space. Depending on how you define an aspect or feature of a situation, problem or thing, these customers can be  quantitatively presented 
+
+Having this is mind we will begin to explore individual RFM components and how to scale and build this matrics from a scratch. 
